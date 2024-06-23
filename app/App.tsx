7b39f7a0878f6fcc6ee5cac7a5f446e799cd7d2f
hyerim03 <chhari0708@naver.com>
2024-06-24 00:10:16 +0900

    initalize project

--- app/App.tsx
+++ app/App.tsx
@@ -6,74 +6,113 @@
  */
 
 import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
 
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import Main from './screens/Main';
-import Bluetooth from './screens/BlueTooth';
-import BottomTabNavigator from './component/BottomTabNavigator';
-import Login from './screens/Login';
-import SignUp from './screens/SignUp';
-import DiaryWrite from './screens/DiaryWrite';
-import DiaryDetail from './screens/DiaryDetail';
-import Scheduler from './screens/Scheduler';
-import HardWare from './screens/HardWare';
-import DiaryList from './screens/DiaryList';
-const Stack = createStackNavigator();
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
-function App(): React.JSX.Element {
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function Section({children, title}: SectionProps): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="BlueTooth" component={Bluetooth}></Stack.Screen>
-        <Stack.Screen
-          name="Login"
-          options={{
-            headerShown: false,
-          }}>
-          {props => <Login {...props} />}
-        </Stack.Screen>
-        <Stack.Screen
-          name="Main"
-          component={BottomTabNavigator}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="SignUp"
-          component={SignUp}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="DiaryList"
-          component={DiaryList}
-          options={{headerShown: false}}
-        />
-
-        <Stack.Screen
-          name="DiaryWrite"
-          component={DiaryWrite}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="DiaryDetail"
-          component={DiaryDetail}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="Scheduler"
-          component={Scheduler}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="HardWare"
-          component={HardWare}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
   );
 }
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
 
 export default App;
