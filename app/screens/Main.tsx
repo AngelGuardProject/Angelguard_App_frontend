8bd8f90525b7320fe34fde0b281008fd3943da16
hyerim03 <chhari0708@naver.com>
2024-06-24 02:38:29 +0900

    tmp, hm getData

--- app/screens/Main.tsx
+++ app/screens/Main.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
 import {Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 function Main() {
+  const [tmp, setTmp] = useState();
+  const [hm, setHm] = useState();
+  useEffect(() => {
+    getTmp();
+  }, []);
+  const getTmp = () => {
+    axios
+      .get('http://louk342.iptime.org:3010/data?uuid=0')
+      .then(res => {
+        console.log(res.data);
+        setTmp(res.data.temp);
+        setHm(res.data.hm);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <Text>메인페이지</Text>
+        <Text>현재 온도 : {tmp}</Text>
+        <Text>현재 습도 : {hm}</Text>
       </View>
     </SafeAreaView>
   );
