c0cc37a0348c8631455346069e8dfd594689be1a
최혜림 <101788177+hyerim03@users.noreply.github.com>
2024-10-20 18:21:38 +0900

    feat: 내 정보 수정 기능 완성

--- app/api/myInfo.api.tsx
+++ app/api/myInfo.api.tsx
@@ -1,5 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
+import {ToastAndroid} from 'react-native';
 
 interface PropsType {
   setName: (name: string) => void;
@@ -9,7 +10,7 @@ interface PropsType {
 export const getMyInfo = async ({setName, setId}: PropsType) => {
   const id = await AsyncStorage.getItem('id');
   const token = await AsyncStorage.getItem('token');
-  console.log(id);
+
   axios
     .get(`http://34.47.76.73:3000/user/myprofile/${id}`, {
       headers: {
@@ -21,4 +22,32 @@ export const getMyInfo = async ({setName, setId}: PropsType) => {
       setId(res.data.data.user_login_id);
     })
     .catch(err => console.log(err));
+};
+
+interface myInfo {
+  pw: string;
+  name: string;
+  id: string;
+}
+
+export const editMyInfo = async ({pw, name, id}: myInfo) => {
+  const token = await AsyncStorage.getItem('token');
+
+  axios
+    .post(
+      `http://34.47.76.73:3000/user/update/${id}`,
+      {pw: pw, username: name},
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    )
+    .then(res => {
+      ToastAndroid.show('내 정보를 수정하였습니다.', ToastAndroid.SHORT);
+    })
+    .catch(err => {
+      console.log(err);
+      ToastAndroid.show('오류가 발생했습니다.', ToastAndroid.SHORT);
+    });
 };
